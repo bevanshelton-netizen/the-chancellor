05fafbe5effd2e423e0814318c51a6990542ac96
bevanshelton-netizen <bevanshelton@gmail.com>
2026-08-11 22:03:30 +0200

    Mount case outcome intelligence routes

--- start.js
+++ start.js
@@ -5,6 +5,7 @@ require('./case-room-routes')(app);
 require('./case-billing-routes')(app);
 require('./notification-routes')(app);
 require('./reputation-routes')(app);
+require('./outcome-routes')(app);
 const communications = require('./communications-routes')(app);
 
 const port = Number(process.env.PORT || 3000);
