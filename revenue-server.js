b7556932639eff2b8e64ffa5d5120fc0d2d1937c
bevanshelton-netizen <bevanshelton@gmail.com>
2026-08-11 20:37:26 +0200

    Start app with follow-on offer revenue routes

---
+++ revenue-server.js
@@ -0,0 +1,14 @@
+require('dotenv').config({ path: require('node:path').join(__dirname, '.env') });
+const app = require('./server');
+require('./offer-routes')(app);
+
+const port = Number(process.env.PORT || 3000);
+const server = app.listen(port, () => console.log(`Growth Desk revenue server ready at http://localhost:${port}`));
+
+function close(signal){
+  console.log(`${signal} received; closing cleanly.`);
+  server.close(()=>process.exit(0));
+  setTimeout(()=>process.exit(1),10000).unref();
+}
+process.on('SIGTERM',()=>close('SIGTERM'));
+process.on('SIGINT',()=>close('SIGINT'));
