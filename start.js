dbb4c8403ad4400a017ed416a5711ade92ba3ae7
bevanshelton-netizen <bevanshelton@gmail.com>
2026-08-12 06:34:26 +0200

    Mount Referral and Partner Engine

--- start.js
+++ start.js
@@ -7,6 +7,7 @@ require('./notification-routes')(app);
 require('./reputation-routes')(app);
 require('./outcome-routes')(app);
 require('./public-proof-routes')(app);
+require('./referral-routes')(app);
 const communications = require('./communications-routes')(app);
 
 const port = Number(process.env.PORT || 3000);
